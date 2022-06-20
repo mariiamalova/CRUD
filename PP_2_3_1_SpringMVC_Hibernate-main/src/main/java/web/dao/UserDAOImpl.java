224055c6d6857e56feb4074e95fc9b4612d09c42
mariiamalova <106603539+mariiamalova@users.noreply.github.com>
2022-06-20 13:50:11 +0300

    Add files via upload

---
+++ PP_2_3_1_SpringMVC_Hibernate-main/src/main/java/web/dao/UserDAOImpl.java
@@ -0,0 +1,46 @@
+package web.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class UserDAOImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+
+        return entityManager.createQuery("SELECT u FROM User u", User.class).getResultList();
+    }
+    @Override
+    public User getUserById(int id) {
+         return entityManager.find(User.class, id);
+    }
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
+
+    }
+    @Override
+    public void removeUser(int id) {
+       Query query = entityManager.createQuery("delete FROM User where id = :id").setParameter("id",id);
+       query.executeUpdate();
+    }
+
+}
